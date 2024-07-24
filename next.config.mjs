5924d4cce40234dd16d3d25e952c8b4fce014743
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-07-24 20:11:14 +0200

    updates to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  async exportPathMap(defaultPathMap) {
-    delete defaultPathMap["/api/auth/[...nextauth]"];
-    delete defaultPathMap["/api/files"];
-    return defaultPathMap;
-  },
 };
 
 export default nextConfig;
