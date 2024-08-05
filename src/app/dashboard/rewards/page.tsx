b428063bc6aa43465145b0dae786c12a1727a1f9
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-08-05 22:40:06 +0200

    updated the rewards page

--- src/app/dashboard/rewards/page.tsx
+++ src/app/dashboard/rewards/page.tsx
@@ -10,8 +10,10 @@ export default async function Rewards() {
   }
 
   return (
-    <div className="flex-1 flex flex-col items-center px-16 py-10 justify-evenly">
-      <p className="heading">Rewards earned</p>
+    <div className="flex-1 flex flex-col items-center px-16 py-10 justify-evenly bg-[#E4EBFA] w-10/12 h-[70vh] ml-3 rounded-3xl">
+      <p className="text-4xl font-black text-transparent from-[#FFD449] to-[#FF007A] bg-gradient-to-r bg-clip-text">
+        REWARDS EARNED
+      </p>
       <UserRewards />
     </div>
   );
--- src/components/UserRewards.tsx
+++ src/components/UserRewards.tsx
@@ -23,19 +23,21 @@ export default function UserRewards() {
   return (
     <>
       <div className="flex flex-row w-full justify-evenly items-center">
-        <div className="flex flex-col text-center space-y-2">
-          <p className="text-lg">Total rewards</p>
-          <div className="border h-0 border-red-500 w-32 mx-auto shadow-sm shadow-red-500"></div>
-          <p className="text-3xl">0 RB</p>
+        <div className="flex flex-col text-center space-y-2 rounded-full bg-[#FFD449] border-2 border-white h-[250px] w-[250px] justify-center items-center text-black">
+          <p className="text-xl">Total rewards</p>
+          <div className="border h-0 border-[#FF007A] w-32 mx-auto shadow-sm shadow-[#FF007A]"></div>
+          <p className="text-3xl">0 WETH</p>
         </div>
-        <div className="flex flex-col text-center space-y-2">
-          <p className="text-lg">Claim rewards</p>
-          <div className="border h-0 border-red-500 w-32 mx-auto shadow-sm shadow-red-500"></div>
-          <p className="text-3xl">0 RB</p>
+        <div className="flex flex-col text-center space-y-2 rounded-full bg-[#FF007A] border-2 border-black h-[250px] w-[250px] justify-center items-center">
+          <p className="text-xl">Claim rewards</p>
+          <div className="border h-0 border-[#FFD449] w-32 mx-auto shadow-sm shadow-[#FFD449]"></div>
+          <p className="text-3xl">0 WETH</p>
         </div>
       </div>
-      <button className="actionButton">CLAIM REWARDS</button>
-      <Convert />
+      <button className="border-[6px] border-[#FF007A] bg-[#D9D9D9] text-2xl text-black rounded-full py-1 px-3">
+        CLAIM REWARDS
+      </button>
+      {/* <Convert /> */}
     </>
   );
 }
