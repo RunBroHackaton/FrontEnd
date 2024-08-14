84f91771476ca7fd00a2956c78b054ed44d4f2c2
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-08-14 09:43:43 +0200

    PrettyInput

--- src/ui/PrettyInput.tsx
+++ src/ui/PrettyInput.tsx
@@ -1,24 +1,58 @@
-"use client"
+"use client";
 
-import { useState, useRef, Dispatch, SetStateAction } from "react"
+import { useState, useRef, Dispatch, SetStateAction } from "react";
 
-export default function PrettyInput({type, name, label, min, max, input, setInput} : {type: string, name: string, label: string, min: number, max: number, input: any, setInput: Dispatch<SetStateAction<any>>}) {
+export default function PrettyInput({
+  type,
+  name,
+  label,
+  min,
+  max,
+  input,
+  setInput,
+}: {
+  type: string;
+  name: string;
+  label: string;
+  min: number;
+  max: number;
+  input: any;
+  setInput: Dispatch<SetStateAction<any>>;
+}) {
+  const [focus, setFocus] = useState(false);
 
-    const [focus, setFocus] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const inputRef = useRef<HTMLInputElement>(null)
-
-    const handleBlur = () => {
-        if (!input && inputRef.current) {
-            inputRef.current.value = "";
-        }
-        setFocus(false)
+  const handleBlur = () => {
+    if (!input && inputRef.current) {
+      inputRef.current.value = "";
     }
+    setFocus(false);
+  };
 
-    return (
-        <div className="prettyInput">
-            <input className="input-wrapper" type={type} name={name} min={type === "number" ? min : undefined} max={type === "number" && max !== 0 ? max : undefined} onChange={(e) => {setInput(e.target.value)}} onFocus={() => {setFocus(true)}} onBlur={handleBlur} ref={inputRef} required />
-            <label className={`input-label ${!focus && input ? "input-label-focus-none" : focus ? "input-label-focus" : ""}`}>{label}</label>
-        </div>
-    )
+  return (
+    <div className="prettyInput">
+      <input
+        className="input-wrapper"
+        type={type}
+        name={name}
+        min={type === "number" ? min : undefined}
+        max={type === "number" && max !== 0 ? max : undefined}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        onFocus={() => {
+          setFocus(true);
+        }}
+        onBlur={handleBlur}
+        ref={inputRef}
+        required
+      />
+      <label
+        className={`input-label ${!focus && input ? "input-label-focus-none" : focus ? "input-label-focus" : ""}`}
+      >
+        {label}
+      </label>
+    </div>
+  );
 }
