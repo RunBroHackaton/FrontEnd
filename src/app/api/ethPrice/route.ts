3ca7b1e2f218c7abea47853b7c7ad8565ce0fdc4
Jaka Potokar <cc.jakapotokar@gmail.com>
2024-08-14 10:52:49 +0200

    added the right ethUsdPrice pool address

--- src/app/api/ethPrice/route.ts
+++ src/app/api/ethPrice/route.ts
@@ -2,14 +2,11 @@ import { NextResponse } from 'next/server';
 const Moralis = require('moralis').default;
 const { EvmChain } = require('@moralisweb3/common-evm-utils');
 
-// Ensure to properly export the GET handler
 export async function GET(req: Request) {
   try {
-    await Moralis.start({
-      apiKey: process.env.NEXT_PUBLIC_MORALIS,
-    });
-    
-    const address = '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599';
+    await initializeMoralis();
+
+    const address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
     const chain = EvmChain.ETHEREUM;
     const response = await Moralis.EvmApi.token.getTokenPrice({
       address,
@@ -18,10 +15,17 @@ export async function GET(req: Request) {
 
     const price = response.toJSON().usdPrice;
 
-    // Return the price as a JSON response
     return NextResponse.json({ price });
   } catch (error) {
     console.error('Error fetching token price:', error);
     return NextResponse.json({ error: 'Failed to fetch token price' }, { status: 500 });
+  }
+}
+
+async function initializeMoralis() {
+  if (!Moralis.Core.isStarted) {
+    await Moralis.start({
+      apiKey: process.env.NEXT_PUBLIC_MORALIS,
+    });
   }
 }
